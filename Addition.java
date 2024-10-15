40624c55171d3e4746e51f2f58e0f57679839499
Sai0045 <sairajabhale@gmail.com>
2024-10-15 14:43:17 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void
+
+            main(String[] args) {
+        int a = 10, b = 20;
+        int sum;
+        sum = a + b;
+        System.out.println("Addition is : " + sum);
+
+    }
+}
